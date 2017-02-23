27e357c6a9306f73d314be24e5d224c328108625
Jiri Slaby <jirislaby@gmail.com>
2017-02-23 15:27:56 +0100

    travis CI: add LLVM 3.8 and 3.9 build tests
    3.9 does not seem to be in deb repos yet.
    3.7 is not in debian repos either (already).
    
    Legacy build system is not supported since 3.8.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- .travis/install-llvm-and-runtime-compiler.sh
+++ .travis/install-llvm-and-runtime-compiler.sh
@@ -4,7 +4,10 @@ set -ev
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get install -y llvm-${LLVM_VERSION} llvm-${LLVM_VERSION}-dev
 
-  sudo apt-get install -y llvm-${LLVM_VERSION}-tools clang-${LLVM_VERSION}
+  sudo apt-get install -y clang-${LLVM_VERSION}
+  if [ "${LLVM_VERSION}" != "3.8" -a "${LLVM_VERSION}" != "3.9" ]; then
+      sudo apt-get install -y llvm-${LLVM_VERSION}-tools
+  fi
   sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-${LLVM_VERSION} 20
   sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-${LLVM_VERSION} 20
 elif [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
