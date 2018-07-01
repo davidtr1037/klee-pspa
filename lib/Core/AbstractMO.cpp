06409d54d1be5f5ec783119c09bfa88b92c0f6fc
David Trabish <davidtr1037@gmail.com>
2018-07-01 09:59:20 +0300

    updating

--- lib/Core/AbstractMO.cpp
+++ lib/Core/AbstractMO.cpp
@@ -99,53 +99,53 @@ NodeID klee::computeAbstractMO(PointerAnalysis *pta,
 
 uint32_t klee::computeAbstractFieldOffset(uint32_t offset,
                                           const Type *moType) {
-    if (moType->isSingleValueType()) {
-      /* there are 2 options in this case:
-         - a pointer to a primitive type
-         - a pointer to an array (which is probably dynamically allocated)
-         so we should be field insensitive here */
-      return 0;
-    }
+  if (moType->isSingleValueType()) {
+    /* there are 2 options in this case:
+       - a pointer to a primitive type
+       - a pointer to an array (which is probably dynamically allocated)
+       so we should be field insensitive here */
+    return 0;
+  }
 
-    StInfo *stInfo = SymbolTableInfo::SymbolInfo()->getStructInfo(moType);
-    if (offset > stInfo->getSize()) {
-      assert(false);
-    }
+  StInfo *stInfo = SymbolTableInfo::SymbolInfo()->getStructInfo(moType);
+  if (offset > stInfo->getSize()) {
+    assert(false);
+  }
 
-    if (isa<ArrayType>(moType)) {
-      FieldLayout &fl = stInfo->getFieldLayoutVec().front();
-      uint32_t modOffset = offset % fl.size;
-      return computeAbstractFieldOffset(modOffset, fl.type);
-    }
+  if (isa<ArrayType>(moType)) {
+    FieldLayout &fl = stInfo->getFieldLayoutVec().front();
+    uint32_t modOffset = offset % fl.size;
+    return computeAbstractFieldOffset(modOffset, fl.type);
+  }
 
-    if (isa<StructType>(moType)) {
-      uint32_t flattenOffset = 0;
-      for (FieldLayout &fl : stInfo->getFieldLayoutVec()) {
-        if (offset < fl.offset) {
-          /* something went wrong... */
-          assert(false);
-        }
-
-        uint32_t unalignedSize, nextOffset;
-        if (offset < fl.offset + fl.size) {
-          unalignedSize = SymbolTableInfo::SymbolInfo()->getTypeSizeInBytes(fl.type);
-          nextOffset = min(unalignedSize - 1, offset - fl.offset);
-          return flattenOffset + computeAbstractFieldOffset(nextOffset, fl.type);
-        }
-
-        StInfo *fieldStInfo = SymbolTableInfo::SymbolInfo()->getStructInfo(fl.type);
-        flattenOffset += fieldStInfo->getFlattenFieldInfoVec().size();
+  if (isa<StructType>(moType)) {
+    uint32_t flattenOffset = 0;
+    for (FieldLayout &fl : stInfo->getFieldLayoutVec()) {
+      if (offset < fl.offset) {
+        /* something went wrong... */
+        assert(false);
       }
 
-      /* we should not get here... */
-      assert(false);
+      uint32_t unalignedSize, nextOffset;
+      if (offset < fl.offset + fl.size) {
+        unalignedSize = SymbolTableInfo::SymbolInfo()->getTypeSizeInBytes(fl.type);
+        nextOffset = min(unalignedSize - 1, offset - fl.offset);
+        return flattenOffset + computeAbstractFieldOffset(nextOffset, fl.type);
+      }
+
+      StInfo *fieldStInfo = SymbolTableInfo::SymbolInfo()->getStructInfo(fl.type);
+      flattenOffset += fieldStInfo->getFlattenFieldInfoVec().size();
     }
 
-    if (isa<FunctionType>(moType)) {
-      /* TODO: is it correct? */
-      return 0;
-    }
-
+    /* we should not get here... */
     assert(false);
+  }
+
+  if (isa<FunctionType>(moType)) {
+    /* TODO: is it correct? */
     return 0;
+  }
+
+  assert(false);
+  return 0;
 }
