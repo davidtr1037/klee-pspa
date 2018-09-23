b5e4de5ea6dfc502cf6d0b5ad27cd8b9cfacdbd3
David Trabish <davidtr1037@gmail.com>
2018-09-23 20:14:45 +0300

    updating

--- tools/klee/main.cpp
+++ tools/klee/main.cpp
@@ -290,7 +290,9 @@ public:
 KleeHandler::KleeHandler(int argc, char **argv)
     : m_interpreter(0), m_pathWriter(0), m_symPathWriter(0), m_infoFile(0),
       m_outputDirectory(), m_numTotalTests(0), m_numGeneratedTests(0),
-      m_pathsExplored(0), m_argc(argc), m_argv(argv) {
+      m_pathsExplored(0),
+      m_recoveryStatesCount(0), m_snapshotsCount(0),
+      m_argc(argc), m_argv(argv) {
 
   // create output directory (OutputDir or "klee-out-<i>")
   bool dir_given = OutputDir != "";
