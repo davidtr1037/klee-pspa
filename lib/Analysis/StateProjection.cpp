f56253589d47226401cf901d87fb0e376456308c
David Trabish <davidtr1037@gmail.com>
2019-03-19 18:26:03 +0200

    refactoring

--- lib/Analysis/StateProjection.cpp
+++ lib/Analysis/StateProjection.cpp
@@ -45,27 +45,8 @@ size_t klee::getFlatModSize(PointerAnalysis *pta,
   std::set<NodeID> mod;
 
   for (auto i : projection.pointsToMap) {
-    NodeID nodeId = i.first;
-    PAGNode *pagNode = pta->getPAG()->getPAGNode(nodeId);
-    ObjPN *obj = dyn_cast<ObjPN>(pagNode);
-    if (!obj) {
-      continue;
-    }
-
-    const MemObj *memObj = obj->getMemObj();
-    const Value *origVal = memObj->getOrigRefVal();
-    if (origVal) {
-      NodeID base = pta->getPAG()->getObjectNode(origVal);
-      if (isa<FIObjPN>(obj)) {
-        mod.insert(pta->getFIObjNode(base));
-      }
-      if (isa<GepObjPN>(obj)) {
-        GepObjPN *gepObj = dyn_cast<GepObjPN>(obj);
-        mod.insert(pta->getGepObjNode(base, gepObj->getLocationSet()));
-      }
-    } else {
-      mod.insert(nodeId);
-    }
+    NodeID nodeId = stripUniqueAS(pta, i.first);
+    mod.insert(nodeId);
   }
 
   return mod.size();
